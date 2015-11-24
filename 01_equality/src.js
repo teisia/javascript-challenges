618e4013762c6b27478d02f8a7d25d7aa05b342b
Teisia Park <teisia@gmail.com>
2015-11-24 16:41:44 -0700

    completed 10/10 tests

--- 01_equality/src.js
+++ 01_equality/src.js
@@ -1,5 +1,37 @@
 module.exports = {
   sameLength: function(string1, string2) {
-    // your code here:
+  if (string1.length == string2.length) {
+  return true;
+  } else {
+  return false;
+  }
+},
+  firstGreater: function(var1, var2) {
+  if (var1 > var2) {
+  return true;
+  } else {
+  return false;
+  }
+},
+  firstGreaterOrEqualTo: function(var1, var2) {
+  if (var1 >= var2) {
+  return true;
+  } else {
+  return false;
+  }
+},
+  firstLengthGreater: function(var1, var2) {
+  if (var1.length > var2.length) {
+  return true;
+  } else {
+  return false;
+  }
+},
+  secondGreater: function(var1, var2) {
+  if (var2 > var1) {
+  return true;
+  } else {
+  return false;
+  }
   }
 }
